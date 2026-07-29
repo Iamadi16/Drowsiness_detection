14e09535ca1631f26edd46831d3db2b5645c9546
Iamadi16 <nasoodim.16@gmail.com>
2026-07-29 19:02:11 +0330

    till real-time

--- drowsiness.py
+++ drowsiness.py
@@ -1,7 +1,17 @@
+from ultralytics import YOLO
 import torch 
 from matplotlib import pyplot as plt
 import numpy as np 
 import cv2
 
 ##Load model
-model = torch.hub.load('ultralytics/yolov5' , 'yolov5s')
+model = YOLO("yolov8n.pt")
+
+##Make detections
+img = '3.jpg'
+results = model(img)
+print(results)
+
+annotated_img = results[0].plot()
+plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)) #colors are different in cv2 and matplot
+plt.show()
