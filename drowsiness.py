88235deb12e008f4e68cae976c04604ca30bacfe
Iamadi16 <nasoodim.16@gmail.com>
2026-07-30 22:23:01 +0330

    real-time

--- drowsiness.py
+++ drowsiness.py
@@ -7,11 +7,24 @@ import cv2
 ##Load model
 model = YOLO("yolov8n.pt")
 
-##Make detections
-img = '3.jpg'
-results = model(img)
-print(results)
+# annotated_img = results[0].plot()
+# plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)) #colors are different in cv2 and matplot
+# plt.show()
 
-annotated_img = results[0].plot()
-plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)) #colors are different in cv2 and matplot
-plt.show()
+#Real-time detections
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    ret , frame = cap.read()
+    if not ret:
+        break
+
+    ##Make detections
+    results = model(frame , verbose = False) #verbos->delete each terminal info
+    annotated_frame = results[0].plot()
+    cv2.imshow("YOLO", annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()
