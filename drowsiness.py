827a0706ff8e0065cff950727ab2739225bc5865
Iamadi16 <nasoodim.16@gmail.com>
2026-08-16 23:29:18 +0330

    rectangle around face

--- drowsiness.py
+++ drowsiness.py
@@ -1,24 +1,102 @@
-from ultralytics import YOLO
 import cv2
+import torch
+import torch.nn as nn
+from torchvision import models , transforms
 
 ##Load model
-model = YOLO("yolov8n.pt")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# annotated_img = results[0].plot()
-# plt.imshow(cv2.cvtColor(annotated_img, cv2.COLOR_BGR2RGB)) #colors are different in cv2 and matplot
-# plt.show()
+model = models.mobilenet_v2(weights=None)
+model.classifier[1] = nn.Linear(
+    model.last_channel,
+    2
+)
 
-#Real-time detections
+model.load_state_dict(torch.load("drowsiness_model.pth" , map_location=device))
+
+model.to(device)
+model.eval()
+
+transform = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.Resize((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize(
+        mean=[0.485, 0.456, 0.406],
+        std=[0.229, 0.224, 0.225]
+    )
+])
+
+##Face detector
+face_cascade = cv2.CascadeClassifier(
+    cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+) 
+
+classes = ["drowsy" , "notdrowsy"]
+
+##Real-time detections
 cap = cv2.VideoCapture(0)
 while cap.isOpened():
     ret , frame = cap.read()
     if not ret:
         break
 
-    ##Make detections
-    results = model(frame , verbose = False) #verbos->delete each terminal info
-    annotated_frame = results[0].plot()
-    cv2.imshow("YOLO", annotated_frame)
+    ##Detect faces
+    gray = cv2.cvtColor(frame , cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        gray,
+        scaleFactor=1.1,
+        minNeighbors=5,
+        minSize=(100, 100)
+    )
+
+    # Draw rectangle around faces
+    for (x, y, w, h) in faces:
+        face = frame[
+            y:y+h,
+            x:x+w
+        ]
+
+        image = transform(frame)
+        image = image.unsqueeze(0)
+        image = image.to(device)
+
+        with torch.no_grad():
+            outputs = model(image)
+
+        probablities = torch.softmax(outputs, dim=1)
+        predicted_class = torch.argmax(outputs , dim=1).item()
+
+        label = classes[predicted_class]
+        confidence = (probablities[0][predicted_class].item() * 100)
+
+        ##Draw rectangle
+        cv2.rectangle(
+            frame,
+            (x,y),
+            (x+w , y+h),
+            (0,255,0),
+            2
+        )
+        # 10. Display Result
+        # =========================
+
+        text = f"{label}: {confidence:.1f}%"
+
+        cv2.putText(
+            frame,
+            text,
+            (x, y - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.8,
+            (0, 255, 0),
+            2
+        )
+
+        cv2.putText(frame , label , (30,50) , cv2.FONT_HERSHEY_SIMPLEX ,
+                    1 , (0,255,0) , 2)
+
+    cv2.imshow("drowsiness detection", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
--- train.py
+++ train.py
@@ -13,6 +13,7 @@ transform = transforms.Compose([
     )
 ])
 
+# Load dataset
 train_dataset = datasets.ImageFolder(
     "dataset/train_data",
     transform=transform
@@ -29,17 +30,18 @@ test_dataset = datasets.ImageFolder(
 )
 
 train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-val_loader = DataLoader(val_dataset, batch_size=32)
-test_loader = DataLoader(test_dataset, batch_size=32)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
 
-# print("Classes:", train_dataset.classes)
-# print("Train Images:", len(train_dataset))
-# print("Validation Images:", len(val_dataset))
-# print("Test Images:", len(test_dataset))
+print("Classes:", train_dataset.class_to_idx)
+print("Train Images:", len(train_dataset))
+print("Validation Images:", len(val_dataset))
+print("Test Images:", len(test_dataset))
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# print(device)
+print(device)
 
+# Model
 model = models.mobilenet_v2(weights="DEFAULT") #Default weights
 
 model.classifier[1] = nn.Linear( #classifier->which class the image belongs to
@@ -49,23 +51,27 @@ model.classifier[1] = nn.Linear( #classifier->which class the image belongs to
 
 model = model.to(device)
 
+# Loss - optimizer
 criterion = nn.CrossEntropyLoss()
 
 optimizer = optim.Adam(
     model.parameters(),
     lr=0.001
 )
-epochs = 3
 
-#train
+# train
+epochs = 10
+best_val_ac = 0.0
+
 for epoch in range(epochs):
 
     model.train()
 
     running_loss = 0
+    correct = 0
+    total = 0
 
     for images, labels in train_loader:
-
         images = images.to(device)
         labels = labels.to(device)
 
@@ -80,9 +86,81 @@ for epoch in range(epochs):
         optimizer.step()
 
         running_loss += loss.item()
+        _, predicted = torch.max(outputs, 1)
 
-    print(f"Epoch {epoch+1}/{epochs}  Loss: {running_loss:.4f}")
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-torch.save(model.state_dict(), "drowsiness_model.pth")
+    train_accuracy = 100 * correct / total
 
-print("Model Saved!")
+    # validation
+    val_correct = 0
+    val_total = 0
+    val_loss = 0.0
+    with torch.no_grad():
+        for images, labels in val_loader:
+
+            images = images.to(device)
+            labels = labels.to(device)
+
+            outputs = model(images)
+
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item()
+
+            _, predicted = torch.max(outputs, 1)
+
+            val_total += labels.size(0)
+            val_correct += (predicted == labels).sum().item()
+
+    val_accuracy = 100 * val_correct / val_total
+
+    print(
+        f"Epoch {epoch + 1}/{epochs} | "
+        f"Train Loss: {running_loss:.4f} | "
+        f"Train Accuracy: {train_accuracy:.2f}% | "
+        f"Val Loss: {val_loss:.4f} | "
+        f"Val Accuracy: {val_accuracy:.2f}%"
+    )
+
+    #save best one
+    if val_accuracy > best_val_ac:
+        best_val_ac = val_accuracy
+        torch.save(
+            model.state_dict(),
+            "drowsiness_model.pth"
+        )
+        print("best model saved.")
+
+# test
+model.load_state_dict(
+    torch.load(
+        "drowsiness_model.pth",
+        map_location= device
+    )
+)
+
+model.eval()
+
+test_correct = 0
+test_total = 0
+
+with torch.no_grad():
+    for images, labels in test_loader:
+
+        images = images.to(device)
+        labels = labels.to(device)
+
+        outputs = model(images)
+
+        _, predicted = torch.max(outputs, 1)
+
+        test_total += labels.size(0)
+        test_correct += (predicted == labels).sum().item()
+
+test_accuracy = 100 * test_correct / test_total
+
+print(f"Test Accuracy: {test_accuracy:.2f}%")
+
+print("Training Finished!")
